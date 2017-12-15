0b6d1044f6687d42f6d1aa38555a42d548b49a41
Kurt McKee <contactme@kurtmckee.org>
2017-12-15 16:03:34 -0600

    Fix install problems created by importing syncopath in setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,46 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from setuptools import find_packages, setup, Command
+import os
 
-import syncopath
+from setuptools import setup
+
+
+# Package meta-data.
+NAME = 'syncopath'
+DESCRIPTION = 'Synchronize the contents from one directory to another.'
+URL = 'https://github.com/kurtmckee/syncopath'
+EMAIL = 'contactme@kurtmckee.org'
+AUTHOR = 'Kurt McKee'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'scandir',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py'), 'rb') as f:
+    blob = f.read().decode('utf-8')
+
+VERSION = blob.split('=', 1)[-1].strip().strip('\'"')
 
 setup(
-    name='syncopath',
-    version=syncopath.__version__,
-    description='Synchronize the contents from one directory to another.',
-    author='Kurt McKee',
-    author_email='contactme@kurtmckee.org',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
     url='https://github.com/kurtmckee/syncopath',
-    packages=['syncopath'],
+    packages=[NAME],
 
-    install_requires=['scandir'],
+    install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license='GPLv3',
     classifiers=[
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython',
